8e52a706c0b901e9a6332e4797ecd4e26101da02
Иванов Евгений Константинович <genom990@mail.ru>
2017-08-24 21:05:46 +0700

    add database connection

---
+++ BadBroker.DataAccess/Entities/RatesPerDate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace BadBroker.DataAccess.Entities
+{
+    public class RatesPerDate
+    {
+        public int ID { get; set; }
+        public string Base { get; set; }
+        public DateTime Date { get; set; }
+        [NotMapped]
+        public Dictionary<string, decimal> Rates { get; set; }
+        [Column("Rates")]
+        public string RatesJson
+        {
+            get => JsonConvert.SerializeObject(Rates);
+            set
+            {
+                Rates = (Dictionary<string, decimal>) JsonConvert.DeserializeObject(value);
+            }
+        }
+    }
+}
