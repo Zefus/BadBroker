358df2ac9686be97db9216565903e767a32b3dba
Иванов Евгений Константинович <genom990@mail.ru>
2019-05-16 15:58:35 +0700

    Add BadBrokerContext

---
+++ BadBroker.DataAccess/BadBrokerContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using BadBroker.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace BadBroker.DataAccess
+{
+    public class BadBrokerContext : DbContext
+    {
+        public DbSet<QuotesData> QuotesData { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("config.js");
+
+            IConfigurationRoot config = builder.Build();
+
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
