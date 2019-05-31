ebc451b036d98724045495c576afb07e398c1112
Иванов Евгений Константинович <genom990@mail.ru>
2019-05-31 13:10:22 +0700

    edit TradeController

--- BadBroker.WebApi/Controllers/TradeController.cs
+++ BadBroker.WebApi/Controllers/TradeController.cs
@@ -14,21 +14,6 @@ namespace BadBroker.WebApi.Controllers
     [Route("api/[controller]")]
     public class TradeController : Controller
     {
-        // GET: api/<controller>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] InputDTO inputDTO)
         {
@@ -44,18 +29,6 @@ namespace BadBroker.WebApi.Controllers
             {
                 throw new NotImplementedException();
             }
-        }
-
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
