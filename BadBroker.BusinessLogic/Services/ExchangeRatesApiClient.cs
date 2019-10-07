bc8fcbeca7d24c479d599d50da513987f1a19960
Иванов Евгений Константинович <genom990@mail.ru>
2019-10-07 20:21:23 +0700

    add score field to InputDTO constructor

--- BadBroker.BusinessLogic/Services/ExchangeRatesApiClient.cs
+++ BadBroker.BusinessLogic/Services/ExchangeRatesApiClient.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using BadBroker.BusinessLogic.ModelsDTO;
 using BadBroker.BusinessLogic.Interfaces;
 using BadBroker.BusinessLogic.Extensions;
 using BadBroker.BusinessLogic.Exceptions;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace BadBroker.BusinessLogic.Services
@@ -21,24 +23,27 @@ namespace BadBroker.BusinessLogic.Services
         {
             try
             {
-                List<RatesDTO> quotes = new List<RatesDTO>();
+                List<RatesDTO> rates = new List<RatesDTO>();
 
                 HttpResponseMessage response;
 
                 using (HttpClient client = new HttpClient())
                 {
                     foreach (DateTime date in dates)
-                    {                        
-                        response = await client.GetAsync($"https://api.exchangeratesapi.io/date={date.ToApiStringFormat()}?base=USD&symbols=RUB,EUR,GBP,JPY");
+                    {
+                        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+                        string apiFormattedDate = date.ToApiStringFormat();                        
+                        string url = $"https://api.exchangeratesapi.io/{apiFormattedDate}?base=USD&symbols=RUB,EUR,GBP,JPY";
+                        response = await client.GetAsync(url);
                         response.EnsureSuccessStatusCode();
                         string responseBody = await response.Content.ReadAsStringAsync();
                         RatesDTO result = JsonConvert.DeserializeObject<RatesDTO>(responseBody);
                         if (result.Success)
-                            quotes.Add(result);
+                            rates.Add(result);
                         else
                             throw new HttpServiceException($"API error. Status code: {result.Error.Code}. Info: {result.Error.Info}");
                     }
-                    return quotes;
+                    return rates;
                 }
             }
             catch (HttpRequestException ex)
