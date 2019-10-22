b0f0b162f31c67087c65db79d4abccddb6e3c000
Иванов Евгений Константинович <genom990@mail.ru>
2019-10-22 13:03:10 +0700

    simplified DateTimeExtensions.ToApiStringFormat

--- BadBroker.BusinessLogic/Extensions/DateTimeExtensions.cs
+++ BadBroker.BusinessLogic/Extensions/DateTimeExtensions.cs
@@ -8,10 +8,7 @@ namespace BadBroker.BusinessLogic.Extensions
     {
         public static string ToApiStringFormat(this DateTime date)
         {
-            string day = date.Day < 10 ? $"0{date.Day}" : date.Day.ToString();
-            string month = date.Month < 10 ? $"0{date.Month}" : date.Month.ToString();
-
-            return $"{date.Year}-{month}-{day}";
+            return date.ToString("yyyy'-'MM'-'dd");
         }
     }
 }
